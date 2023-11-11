ea3aac606368fc7e13c0ef2650c612faf4525d7f
nexus122 <juan-pablo122@hotmail.es>
2023-11-11 15:49:59 +0100

    Subir Variables de entorno

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  supabase: {
+    publicKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im56dnViZnlwc2h5bmdidnhwYmNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgzNTQwMTEsImV4cCI6MjAxMzkzMDAxMX0.Uu_GLCByafbqx1dnTuc4lDT59B6Q8uWLZa0puWZfDtc",
+    url: "https://nzvubfypshyngbvxpbcb.supabase.co"
+  }
 };
+
+export const USER_STORAGE_KEY = 'sb-nzvubfypshyngbvxpbcb-auth-token';
